60709a6bfe6a63d025150d6946fc3037812397c5
Codeaddictjaver <102610715+Javernick@users.noreply.github.com>
2022-07-13 18:44:48 +0300

    commit on added-list

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './index.css';
+
+let wussghostman = document.querySelector('h1');
+wussghostman.classList.add('h1');
+console.log('wuss wuss')
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  performance: {
+    hints: false,
+  },
+};
