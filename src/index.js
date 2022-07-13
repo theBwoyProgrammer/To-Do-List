bf47470edfd5bbf47daa96a6e34cdd0ed924e338
Codeaddictjaver <102610715+Javernick@users.noreply.github.com>
2022-07-14 00:34:08 +0300

    commit on added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,57 @@
 import './index.css';
 
-let wussghostman = document.querySelector('h1');
-wussghostman.classList.add('h1');
-console.log('wuss wuss')
+const list = [
+{
+    description: 'cook',
+    completed: false,
+    index: 0,
+},
+{
+    description: 'cook',
+    completed: false,
+    index: 0,
+},
+{
+    description: 'cook',
+    completed: false,
+    index: 0,
+},
+{
+    description: 'cook',
+    completed: false,
+    index: 0,
+},
+{
+    description: 'cook',
+    completed: false,
+    index: 0,
+},
+{
+    description: 'cook',
+    completed: false,
+    index: 0,
+},
+];
+
+const unorderedList = document.querySelector('.list-items');
+
+for(let i = 0; i<list.length; i+=1){
+const listMom = document.createElement('li');
+listMom.setAttribute('class', 'list');
+
+const input = document.createElement('input');
+input.setAttribute('contenteditable', false);
+input.setAttribute('type', 'checkbox');
+
+const span = document.createElement('span');
+span.setAttribute('contenteditable', true);
+
+const icon = document.createElement('i');
+icon.setAttribute('class', 'bi bi-three-dots-vertical');
+icon.setAttribute('id', 'three-dots');
+
+span.textContent += list[i].description;
+listMom.append(input, span, icon);
+unorderedList.append(listMom);
+}
+
